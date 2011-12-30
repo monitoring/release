9cd734864049e8a11250ae953c790a074198604a
Manuel Rodriguez <manuel.rodriguez.mail@gmail.com>
2011-12-31 00:46:54 +0100

    Update

--- app/Controller/RequestsController.php
+++ app/Controller/RequestsController.php
@@ -6,7 +6,19 @@ class RequestsController extends AppController {
 	
 	public function index() {
 		$this->set('requests', $this->Request->find('all'));
-		$this->set('tweets', $this->Tweet->find('all'));
+	}
+	
+	public function view($id = null) {
+	   	$this->Request->id = $id;
+	    $this->set('request', $this->Request->read());
+	
+		$this->set('included_kws', $this->Keyword->find('list', array(
+		        'conditions' => array('Keyword.request_id' => $id, 'Keyword.isIncluded' => 1),
+				'fields' => array('Keyword.value'))));
+		
+		$this->set('excluded_kws', 	$this->Keyword->find('list', array(
+			        'conditions' => array('Keyword.request_id' => $id, 'Keyword.isIncluded' => 0),
+					'fields' => array('Keyword.value'))));
 	}
 }
 ?>
